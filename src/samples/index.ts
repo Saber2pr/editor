7cb6ad38d0d15a3c50e48e7bdda03e9cb36ad2fe
saber2pr <saber2pr@gmail.com>
2020-04-23 16:38:40 +0000

    fixed word spell

--- src/samples/index.ts
+++ src/samples/index.ts
@@ -6,14 +6,14 @@ interface Defaults {
 }
 
 export const loadSamples = async (): Promise<Defaults> => {
-  const html = await fetch("/libs/simples/index.html").then(res => res.text())
-  const javascript = await fetch("/libs/simples/main.js").then(res =>
+  const html = await fetch("/libs/samples/index.html").then(res => res.text())
+  const javascript = await fetch("/libs/samples/main.js").then(res =>
     res.text()
   )
-  const typescript = await fetch("/libs/simples/main.tsx").then(res =>
+  const typescript = await fetch("/libs/samples/main.tsx").then(res =>
     res.text()
   )
-  const css = await fetch("/libs/simples/style.css").then(res => res.text())
+  const css = await fetch("/libs/samples/style.css").then(res => res.text())
 
   return {
     javascript,
