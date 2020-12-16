c7193d0e94883f2f6300de2543dc268de4b30829
saber2pr <1029985799@qq.com>
2020-12-16 15:53:00 +0800

    feat

--- src/core/options.ts
+++ src/core/options.ts
@@ -4,21 +4,22 @@
  * @Last Modified by: saber2pr
  * @Last Modified time: 2020-05-05 21:10:05
  */
-import { monaco } from "./monaco"
+import { monaco } from './monaco'
 
 export const commonOptions = {
-  tabSize: 2
+  tabSize: 2,
 }
 
 export const tsxCompilerOptions = {
-  jsx: monaco.languages.typescript.JsxEmit["React"],
-  target: monaco.languages.typescript.ScriptTarget["ES5"],
-  module: monaco.languages.typescript.ModuleKind["AMD"],
+  jsx: monaco.languages.typescript.JsxEmit['React'],
+  target: monaco.languages.typescript.ScriptTarget['ES5'],
+  module: monaco.languages.typescript.ModuleKind['AMD'],
   allowSyntheticDefaultImports: true,
   esModuleInterop: true,
   allowJs: true,
   experimentalDecorators: true,
   emitDecoratorMetadata: true,
   downlevelIteration: true,
-  removeComments: true
+  removeComments: true,
+  lib: ['dom', 'dom.iterable', 'esnext'],
 }
